f37de01ecf2fa659a7ebe2bc32e4a48ca1d09117
laron98 <72889951+laron98@users.noreply.github.com>
2023-01-25 16:53:21 -0800

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,5 +9,6 @@
 #define ALPHABET_SIZE 26
 
 // TODO: functions declarations go below
-
+void vowAndCon(int vowels, int consonant);
+void displayHistogram(int most, int histogram[]);
 #endif /* STATS_H_ */
