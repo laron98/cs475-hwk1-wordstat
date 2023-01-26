85a80071b4d6f1c16c9887eba3be616ff752347f
laron98 <72889951+laron98@users.noreply.github.com>
2023-01-25 16:50:36 -0800

    Update stats.c

--- stats.c
+++ stats.c
@@ -10,12 +10,12 @@
 #include "stats.h"
 
 // TODO: function definitions for dealing with histogram below
-displayHistogram(int most, int histogram[]){
+void displayHistogram(int most, int histogram[]){
     int c;
     int r; 
     for(r = most; r > 0; r--){
-        for(c = 0; c < MAX_INPUT_LEN; c++){
-            if(histogram[c] >= c){
+        for(c = 0; c < ALPHABET_SIZE; c++){
+            if(histogram[c] >= r){
                 printf("* ");
             } else{
                 printf("  ");
@@ -23,16 +23,16 @@ displayHistogram(int most, int histogram[]){
         }
         printf("\n");
     }
-    printf("a b c d e f g h i j k l m n o p q r s t u v w x y z");
-    for(c = 0; c < MAX_INPUT_LEN; c++){
+    printf("a b c d e f g h i j k l m n o p q r s t u v w x y z\n");
+    for(c = 0; c < ALPHABET_SIZE; c++){
         printf("%d ", histogram[c]);
-    }
+    } 
     printf("\n");
 }
 
-vowAndCon(int vowels, int consonant){
+void vowAndCon(int vowels, int consonant){
     int total = consonant + vowels;
-    float perVow = vowels / total;
-    float perCon = consonant / total;
+    float perVow = (float)vowels / (float)total;
+    float perCon = (float)consonant / (float)total;
     printf("Vowels = %d (%0.3f%%), Consonants = %d (%0.3f%%), Total= %d\n", vowels, perVow, consonant, perCon, total);
 }
